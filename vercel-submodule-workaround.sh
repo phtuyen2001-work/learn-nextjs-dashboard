9ecc858315af29223db1f0b4298056fef76bdce0
Tuyen AIOZ <tuyen.hon.pham@aioz.io>
2025-01-09 16:08:22 +0700

    try again with ssh url

--- vercel-submodule-workaround.sh
+++ vercel-submodule-workaround.sh
@@ -3,6 +3,7 @@
 set -Eeuo pipefail
 
 GITMODULES=".gitmodules"
+FEXT=".bak"
 GITMODULES_BACKUP="${GITMODULES}${FEXT}"
 
 function cleanup {
@@ -21,7 +22,7 @@ function submodule_workaround {
   fi
 
   echo "Monkey patching..."
-  sed -i -E "s/git@github.com:/https:\/\/${GITHUB_REPO_CLONE_TOKEN}@github.com\//" "$GITMODULES"
+  sed -i"$FEXT" "s/git@github.com:/https:\/\/$GITHUB_REPO_CLONE_TOKEN@github.com\//" "$GITMODULES"
   echo "Done!"
 
   echo "Synchronising submodules' remote URL configuration..."
@@ -29,7 +30,7 @@ function submodule_workaround {
   echo "Done!"
 
   echo "Updating the registered submodules to match what the superproject expects..."
-  git submodule update --init --recursive
+  git submodule update --init --recursive --jobs "$(getconf _NPROCESSORS_ONLN)"
   echo "Done!"
 }
 
