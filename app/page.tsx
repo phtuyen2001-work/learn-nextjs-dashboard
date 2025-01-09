f4100270ec2f48adf36e93a3f38f5f0495d7dbc7
Tuyen AIOZ <tuyen.hon.pham@aioz.io>
2025-01-09 14:46:24 +0700

    trigger a deployment

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Image from 'next/image';
 import { addOne } from '../test-private-submodule/utils/add.js'
 
 export default function Page() {
+  
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
