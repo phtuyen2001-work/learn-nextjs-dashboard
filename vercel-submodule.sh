b9c88fc08b087a706f1370fc65bcf3d39f1c0010
Tuyen AIOZ <tuyen.hon.pham@aioz.io>
2025-01-09 14:38:40 +0700

    try another workaround

--- vercel-submodule.sh
+++ vercel-submodule.sh
@@ -1,39 +1,37 @@
-# github submodule repo address without https:// prefix
-SUBMODULE_GITHUB=github.com/phtuyen2001-work/test-private-submodule
+#!/usr/bin/env bash
 
-# .gitmodules submodule path
-SUBMODULE_PATH=test-private-submodule
+set -Eeuo pipefail
 
-# github access token is necessary
-# add it to Environment Variables on Vercel
-if [ "$GH_PAT" == "" ]; then
-  echo "Error: GH_PAT is empty"
-  exit 1
-fi
+GITMODULES=".gitmodules"
+FEXT=".bak"
+GITMODULES_BACKUP="${GITMODULES}${FEXT}"
 
-# stop execution on error - don't let it build if something goes wrong
-set -e
+function cleanup {
+  echo "Cleaning the runner..."
+  rm -f "$GITMODULES" "$GITMODULES_BACKUP"
+  git restore "$GITMODULES"
+  echo "Done!"
+}
 
-# get submodule commit
-output=`git submodule status --recursive` # get submodule info
-no_prefix=${output#*-} # get rid of the prefix
-COMMIT=${no_prefix% *} # get rid of the suffix
+trap cleanup EXIT
 
-# set up an empty temporary work directory
-rm -rf tmp || true # remove the tmp folder if exists
-mkdir tmp # create the tmp folder
-cd tmp # go into the tmp folder
+function submodule_workaround {
+  if [ "$GITHUB_REPO_CLONE_TOKEN" == "" ]; then
+    echo "GITHUB_REPO_CLONE_TOKEN is empty!"
+    exit 1
+  fi
 
-# checkout the current submodule commit
-git init # initialise empty repo
-git remote add origin https://$GH_PAT@$SUBMODULE_GITHUB # add origin of the submodule
-git fetch --depth=1 origin $COMMIT # fetch only the required version
-git checkout $COMMIT # checkout on the right commit
+  echo "Monkey patching..."
+  sed -i"$FEXT" "s/git@github.com:/https:\/\/oauth2:${GITHUB_REPO_CLONE_TOKEN}@github.com\//" "$GITMODULES"
+  echo "Done!"
 
-# move the submodule from tmp to the submodule path
-cd .. # go folder up
-rm -rf tmp/.git # remove .git 
-mv tmp/* $SUBMODULE_PATH/ # move the submodule to the submodule path
+  echo "Synchronising submodules' remote URL configuration..."
+  git submodule sync
+  echo "Done!"
 
-# clean up
-rm -rf tmp # remove the tmp folder
+  echo "Updating the registered submodules to match what the superproject expects..."
+  git submodule update --init --recursive --jobs "$(getconf _NPROCESSORS_ONLN)"
+  echo "Done!"
+}
+
+submodule_workaround
