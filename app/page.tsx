063940ac6bc2a679ab62f734ef424acac4380b30
Tuyen AIOZ <tuyen.hon.pham@aioz.io>
2025-01-09 11:29:03 +0700

    update private submodule and test it

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
+import { addOne } from '../test-private-submodule/utils/add.js'
 
 export default function Page() {
   return (
@@ -15,6 +16,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={styles.shape}></div>
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            <h1>HERE IS addOne: {addOne(1)}</h1>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
